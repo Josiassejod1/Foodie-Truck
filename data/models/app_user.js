f21eb1ff5acd4c0a1d1bfe6c28c19a2272928896
dalvin <dalvin.josias@gmail.com>
2021-03-10 00:03:58 -0800

    First commit

---
+++ data/models/app_user.js
@@ -0,0 +1,15 @@
+import { Model } from "./base";
+
+export class AppUser extends Model {
+    get defaults() {
+        return {
+            firstName: "",
+            lastName: "",
+            isRestaurant?: "",
+            address: [],
+            phone: "",
+            role: "customer"
+
+        }
+    }
+}
